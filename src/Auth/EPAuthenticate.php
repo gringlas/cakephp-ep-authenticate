2962176bafedbe81489174f98977db2bc08a87fc
Sebastian Köller <mail@sebastian-koeller.de>
2018-07-14 12:39:38 +0200

    first commit

---
+++ src/Auth/EPAuthenticate.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: sebastiankoller
+ * Date: 14.07.18
+ * Time: 10:19
+ */
+
+namespace gringlas\EPAuth\Auth;
+
+
+use Cake\Auth\BaseAuthenticate;
+use Cake\Controller\ComponentRegistry;
+use Cake\Core\Configure;
+use Cake\Http\Response;
+use Cake\Http\ServerRequest;
+use Cake\Network\Exception\UnauthorizedException;
+use Cake\ORM\Locator\LocatorAwareTrait;
+
+class EPAuthenticate extends BaseAuthenticate
+{
+
+    use LocatorAwareTrait;
+
+
+    public function __construct(ComponentRegistry $registry, array $config = [])
+    {
+        $defaultConfig = [
+            'debugOnly' => true,
+            'header' => 'EP-Authorization',
+            'userId' => 1,
+            'unauthenticatedException' => UnauthorizedException::class,
+        ];
+
+        if (!class_exists(UnauthorizedException::class)) {
+            $defaultConfig['unauthenticatedException'] = 'Cake\Network\Exception\UnauthorizedException';
+        }
+
+        $this->setConfig($defaultConfig);
+
+        parent::__construct($registry, $config);
+    }
+
+
+    public function authenticate(ServerRequest $request, Response $response)
+    {
+        return $this->getUser($request);
+    }
+
+    public function getUser(ServerRequest $request)
+    {
+        $config = $this->_config;
+        if ($this->checkDebugOnly() && $this->checkHeader($request)) {
+            $user = $this->getTableLocator()->get($config['userModel'])
+                ->get($this->getConfig('userId'));
+            return $user->toArray();
+        }
+        return false;
+    }
+
+
+    private function checkDebugOnly()
+    {
+        if ($this->getConfig('debugOnly')) {
+            return Configure::read('debug');
+        }
+        return true;
+    }
+
+
+    private function checkHeader(ServerRequest $request)
+    {
+        $header = $request->getHeader($this->getConfig('header'));
+        $passwordCheck = $request->getHeaderLine($this->getConfig('header')) == $this->getConfig('password');
+        return ($header && $passwordCheck);
+    }
+}
---
+++ tests/TestCase/Auth/EPAuthenticateTest.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: sebastiankoller
+ * Date: 14.07.18
+ * Time: 11:46
+ */
+
+namespace gringlas\EPAuthenticate\Auth\Test\TestCase\Auth;
+
+
+use Cake\Controller\ComponentRegistry;
+use Cake\Http\Response;
+use Cake\Http\ServerRequest;
+use Cake\TestSuite\TestCase;
+use gringlas\EPAuth\Auth\EPAuthenticate;
+
+class EPAuthenticateTest extends TestCase
+{
+
+    /**
+     * @var EPAuthenticate
+     */
+    private $EPAuthenticate;
+
+
+    private $request;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->Registry = new ComponentRegistry();
+        $this->Auth = new EPAuthenticate($this->Registry, [
+        ]);
+        $this->Registry->Auth = $this->auth;
+        $this->Response = $this->getMockBuilder(Response::class)
+            ->getMock();
+
+    }
+
+
+    public function testGetUser()
+    {
+        $request = new ServerRequest('posts/index');
+        $auth = new EPAuthenticate($this->Registry);
+        $this->assertTrue($auth->getUser($request));
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+        // Clean up after we're done
+        unset($this->component, $this->controller);
+    }
+}
